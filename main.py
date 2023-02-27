5931f624ee92fca3150b2b89409cae746fe33a00
hesam21188 <hosseinharatii@gmail.com>
2023-02-27 20:48:17 +0330

    make a triangle

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from PIL import ImageTk, Image
 score = 0
 
 def make_equation():
-    a = rm.randint(1,10)
-    b = rm.randint(1,10)
-    angle_1.config(text=a)
-    angle_2.config(text=b)
-    make_equation.c = (a**2)+(b**2)
-
+    make_equation.a = rm.randint(11,25)
+    make_equation.b = rm.randint(5,11)
+    angle_1.config(text=make_equation.a)
+    angle_2.config(text=make_equation.b,)
+    make_equation.c = (make_equation.a**2)+(make_equation.b**2)
+    yi = 10.45*make_equation.b
+    canvas_window.delete("all")
+    canvas_window.create_polygon([250,10,250,200,yi,200],outline='black',fill='gray', width=2)
 
 def submit():
     answer = inp.get()
@@ -34,9 +36,11 @@ def handler(e):
 window = Tk()
 window.title("Pythagoras")
 window.iconbitmap("icon.ico")
+canvas_window = Canvas(window, width=259, height=209)
+canvas_window.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-triangle = ImageTk.PhotoImage(Image.open("triangle.png"))
-img_triangle = Label(window, image = triangle)
+#triangle = ImageTk.PhotoImage(Image.open("triangle.png"))
+#img_triangle = Label(window, image = triangle)
 angle_1 = Label(window,text="",font="arial 20")
 angle_2 = Label(window,text="",font="arial 20")
 hypotenuse = Label(window,text="X",font="arial 20")
@@ -48,17 +52,24 @@ radical = Label(window,text="√",font="arial 32 bold")
 socor = Label(window,text="Score : 0",font="arial 16",bg="black",fg="yellow")
 
 
-img_triangle.place(relx=0.5, rely=0.5, anchor=CENTER)
+#img_triangle.place(relx=0.5, rely=0.5, anchor=CENTER)
+make_equation()
 angle_1.place(relx=0.5, rely=0.5,y=-25,x=-25, anchor=CENTER)
 angle_2.place(relx=0.5, rely=0.5,y=125,x=5, anchor=CENTER)
-hypotenuse.place(relx=0.5, rely=0.5,y=20,x=160, anchor=CENTER)
+hypotenuse.place(relx=0.5, rely=0.5,y=0,x=160, anchor=CENTER)
 inp.place(relx=0.5, rely=0.5,y=185,x=5, anchor=CENTER)
 inp_text.place(relx=0.5, rely=0.5,y=185,x=-165, anchor=CENTER)
 radical.place(relx=0.5, rely=0.5,y=185,x=-140, anchor=CENTER)
 btn_ok.place(relx=0.5, rely=0.5,y=235,x=5, anchor=CENTER)
 btn_ref.place(relx=0.5, rely=0.5,y=270,x=5, anchor=CENTER)
 socor.place(relx=0.5, rely=0.5,y=-200, anchor=CENTER)
-make_equation()
+
+
+
+yi = 10.45*make_equation.b
+canvas_window.create_polygon([250,10,250,200,yi,200],outline='black',fill='gray', width=2)
+
+
 window.bind('<Return>',handler)
 
 
